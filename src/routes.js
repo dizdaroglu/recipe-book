aae423c0f8f9ec31c79165e1d3f5093bd108dbcf
dizdaroglu <furkndizdaroglu@gmail.com>
2020-02-06 14:22:15 +0300

    initial

---
+++ src/routes.js
@@ -0,0 +1,50 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+
+import HomeScreen from './screens/HomeScreen';
+import SplashScreen from './screens/SplashScreen';
+import AuthScreen from './screens/AuthScreen';
+import AddCook from './screens/CookScreen/AddCook';
+import UserCook from './screens/CookScreen/UserCook';
+
+
+
+
+
+const HomeStack = createStackNavigator({
+    Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+    Auth: AuthScreen
+})
+const AddCookStack = createStackNavigator({
+    AddCook: AddCook
+})
+const UserCookStack = createStackNavigator({
+    UserCook: UserCook
+})
+
+const AppBottom = createBottomTabNavigator({
+    Home: {
+        screen: HomeStack
+    },
+    Add: {
+        screen: AddCookStack
+    }
+})
+
+const AppDrawer = createDrawerNavigator({
+    AppBottom: AppBottom,
+    User: UserCookStack
+})
+
+export default createAppContainer(createSwitchNavigator({
+    App: AppDrawer,
+    Auth: AuthStack,
+    Splash: SplashScreen
+}, {
+    initialRouteName: 'App'
+}))
